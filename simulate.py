ef726dfdd208a024c48928310bc6b558367da0b9
Robert Schmid <r.schmid@outlook.com>
2019-06-25 14:35:03 +0200

    Improved tip selection

--- simulate.py
+++ simulate.py
@@ -6,16 +6,110 @@ import tensorflow as tf
 import networkx as nx
 import matplotlib.pyplot as plt
 
+from multiprocessing.dummy import Pool
+from itertools import starmap
+
 from tensorflow_federated import python as tff
 
 NUM_EPOCHS = 10
 BATCH_SIZE = 20
 SHUFFLE_BUFFER = 500
+BATCHES_PER_ROUND = 100
 
 NUM_CLIENTS = 5
 
+ALPHA = 0.2
 
-class Tip:
+
+class TipSelector:
+    def __init__(self, tangle):
+        self.tangle = tangle
+
+    def tip_selection(self):
+        entry_point = self.tangle.genesis
+
+        entry_point_trunk = entry_point
+        entry_point_branch = entry_point
+
+        # Build a map of transactions that directly approve a given transaction
+        approving_transactions = {x: [] for x in self.tangle.transactions}
+        for x in self.tangle.transactions:
+            if x.p1 is not None:
+                approving_transactions[x.p1].append(x)
+            if x.p2 is not None and x.p1 != x.p2:
+                approving_transactions[x.p2].append(x)
+
+        ratings = self.calculate_cumulative_weight(approving_transactions)
+
+        trunk = self.walk(entry_point_trunk, ratings, approving_transactions)
+        branch = self.walk(entry_point_branch, ratings, approving_transactions)
+
+        return trunk, branch
+
+    def calculate_cumulative_weight(self, approving_transactions):
+        rating = {}
+        for tx in self.tangle.transactions:
+            rating[tx] = len(self.future_set(tx, approving_transactions)) + 1
+
+        return rating
+
+    def future_set(self, tx, approving_transactions):
+        direct_approvals = approving_transactions[tx]
+        indirect_approvals = [self.future_set(x, approving_transactions) for x in direct_approvals]
+        return {id(approver) for s in [direct_approvals, indirect_approvals] for approver in s}
+
+    def walk(self, tx, ratings, approving_transactions):
+        step = tx
+        prev_step = None
+
+        while step:
+            approvers = approving_transactions[step]
+            prev_step = step
+            step = self.next_step(ratings, approvers)
+
+        # When there are no more steps, this transaction is a tip
+        return prev_step
+
+    def next_step(self, ratings, approvers):
+        approvers_with_rating = approvers  # There is a rating for every possible approver
+
+        # There is no valid approver, this transaction is a tip
+        if len(approvers_with_rating) == 0:
+            return None
+
+        approvers_ratings = [ratings[a] for a in approvers_with_rating]
+        weights = self.ratings_to_weight(approvers_ratings)
+        approver = self.weighted_choice(approvers_with_rating, weights)
+
+        return approver
+
+        # Skip validation
+        # if approver is not None:
+        #     tail = validator.findTail(approver)
+        #
+        #     # If the selected approver is invalid, step back and try again
+        #     if validator.isInvalid(tail):
+        #         approvers = approvers.remove(approver)
+        #
+        #         return self.next_step(ratings, approvers)
+        #
+        #     return tail
+        #
+        # return None
+
+    @staticmethod
+    def weighted_choice(approvers, weights):
+        total_weight = sum(weights)
+        return np.random.choice(approvers, p=[w / total_weight for w in weights])
+
+    @staticmethod
+    def ratings_to_weight(ratings):
+        highest_rating = max(ratings)
+        normalized_ratings = [r - highest_rating for r in ratings]
+        return [np.exp(r * ALPHA) for r in normalized_ratings]
+
+
+class Transaction:
     def __init__(self, weights, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -30,18 +124,36 @@ class Tip:
 
 class Tangle:
     def __init__(self, tip):
-        self.tips = [tip]
+        self.genesis = tip
+        self.transactions = [self.genesis]
 
     def choose_tips(self):
-        if len(self.tips) < 2:
-            return tuple([self.tips[0], self.tips[0]])
+        if len(self.transactions) < 2:
+            return tuple([self.genesis, self.genesis])
 
-        total_height = sum([2**x.height for x in self.tips])
-        return tuple(np.random.choice(self.tips, 2, p=[(2**x.height) / total_height for x in self.tips], replace=False))
+        selector = TipSelector(self)
+        return selector.tip_selection()
 
-    def add_tip(self, tip):
+    def add_transaction(self, tip):
         print("Adding tip")
-        self.tips.append(tip)
+        self.transactions.append(tip)
+
+    def show(self):
+        graph = nx.DiGraph()
+
+        graph.add_edges_from([(id(x), id(x.p1)) for x in self.transactions if x.p1 is not None])
+        graph.add_edges_from([(id(x), id(x.p2)) for x in self.transactions if x.p2 is not None])
+
+        val_map = {id(x): x.height for x in self.transactions}
+        values = [val_map.get(node) for node in graph.nodes()]
+
+        # Need to create a layout when doing
+        # separate calls to draw nodes and edges
+        pos = nx.spring_layout(graph)
+        nx.draw_networkx_nodes(graph, pos, cmap=plt.get_cmap('jet'), node_color=values, node_size=100)
+        nx.draw_networkx_labels(graph, pos)
+        nx.draw_networkx_edges(graph, pos, edgelist=graph.edges(), arrows=False)
+        plt.show()
 
 
 class Model:
@@ -97,7 +209,6 @@ class Model:
         return model
 
     def train(self, data):
-        BATCHES_PER_ROUND = 100
         self.model.fit(self.preprocess(data), steps_per_epoch=BATCHES_PER_ROUND, epochs=NUM_EPOCHS, verbose=0)
 
     def evaluate(self, data):
@@ -109,12 +220,11 @@ class Model:
 
 
 class Node:
-    def __init__(self, data, tangle):
+    def __init__(self, tangle):
         self.model = Model()
-        self.data = data
         self.tangle = tangle
 
-    def process_next_batch(self):
+    def process_next_batch(self, data):
         # Obtain two tips from the tangle
         tip1, tip2 = self.tangle.choose_tips()
         print(f"Got tips with heights {tip1.height} and {tip2.height}")
@@ -122,30 +232,14 @@ class Node:
         # Perform averaging
         averaged_model = Model(tip1.weights).average(Model(tip2.weights))
 
-        averaged_model.train(self.data)
+        averaged_model.train(data)
 
-        if averaged_model.performs_better_than(self.model, self.data):
+        if averaged_model.performs_better_than(self.model, data):
             self.model = averaged_model
 
-            self.tangle.add_tip(Tip(self.model.get_weights(), tip1, tip2))
+            return Transaction(self.model.get_weights(), tip1, tip2)
 
-
-def show(tangle):
-    G = nx.DiGraph()
-
-    G.add_edges_from([(id(x), id(x.p1)) for x in tangle.tips if x.p1 is not None])
-    G.add_edges_from([(id(x), id(x.p2)) for x in tangle.tips if x.p2 is not None])
-
-    val_map = {id(x): x.height for x in tangle.tips}
-    values = [val_map.get(node) for node in G.nodes()]
-
-    # Need to create a layout when doing
-    # separate calls to draw nodes and edges
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color=values, node_size=100)
-    nx.draw_networkx_labels(G, pos)
-    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=False)
-    plt.show()
+        return None
 
 
 if __name__ == '__main__':
@@ -153,13 +247,27 @@ if __name__ == '__main__':
     emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
 
     # Create tangle
-    tangle = Tangle(Tip(Model().get_weights(), None, None))
+    tangle = Tangle(Transaction(Model().get_weights(), None, None))
 
     # Create clients
     sample_clients = emnist_train.client_ids[0:NUM_CLIENTS]
-    nodes = [Node(emnist_test.create_tf_dataset_for_client(x), tangle) for x in sample_clients]
+    sample_datasets = [emnist_test.create_tf_dataset_for_client(x) for x in sample_clients]
+    nodes = [Node(tangle) for x in sample_clients]
 
     for _ in range(10):
-        for i in range(NUM_CLIENTS):
-            nodes[i].process_next_batch()
-        show(tangle)
+        def process_next_batch(node, data):
+            return node.process_next_batch(data)
+
+        new_transactions = list(starmap(process_next_batch, [(nodes[i], sample_datasets[i]) for i in range(NUM_CLIENTS)]))
+        for t in new_transactions:
+            if t is not None:
+                tangle.add_transaction(t)
+
+        # with Pool(NUM_CLIENTS) as p:
+        #     new_transactions = p.starmap(process_next_batch, [(nodes[i], sample_datasets[i]) for i in range(NUM_CLIENTS)])
+        #
+        #     for t in new_transactions:
+        #         if t is not None:
+        #             tangle.add_transaction(t)
+
+        tangle.show()
