9f58764816ca2823603fd9fe972c0e211a019d41
Jasper Blum <Jasper.Blum@vm-gpu02.dhclab.i.hpi.de.de>
2020-02-17 12:58:08 +0000

    Update leaf, add experiments.py

--- experiments.py
+++ experiments.py
@@ -15,7 +15,6 @@ params = {
 }
 
 for p in ParameterGrid(params):
-    print(p)
     shutil.rmtree('/tangle_data', ignore_errors=True)
     os.system("python3 main.py -dataset %s -model %s --num-rounds %s --eval-every %s --clients-per-round %s --num-tips %s --sample-size %s --reference-avg-top %s --target-accuracy %s -lr %s" %
         (p['dataset'], p['model'], p['num_rounds'], p['eval_every'], p['clients_per_round'], p['num_tips'], p['sample_size'], p['reference_avg_top'], p['target_accuracy'], p['learning_rate']))
