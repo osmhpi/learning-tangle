5045c4f190c7848d8ab22f8d27789b26f28f028b
Bjarne Pfitzner <Bjarne.Pfitzner@hpi.de>
2020-02-18 09:56:10 +0000

    added labelflip poisoning output to experiments and results file

--- experiments.py
+++ experiments.py
@@ -5,22 +5,25 @@ from sklearn.model_selection import ParameterGrid
 params = {
     'dataset': ['femnist'],
     'model': ['cnn'],
-    'num_rounds': [100],
+    'num_rounds': [200],
     'eval_every': [10],
     'clients_per_round':  [15],
-    'num_tips':  [2,3,5],
-    'sample_size':  [2,5,10],
+    'num_tips':  [2],
+    'sample_size':  [10],
     'reference_avg_top':  [1,2,10,50,100],
-    'target_accuracy':  [0.7],
-    'learning_rate':  [0.06]
+    'target_accuracy':  [1.0],
+    'learning_rate':  [0.06],
+    'poison_type':  ['LABELFLIP'],
+    'poison_fraction':  [0.1],
+    'poison_from':  [100],
 }
 
 file_name = 'results.txt'
     
 for p in ParameterGrid(params):
     os.system('rm -rf tangle_data')
-    command = "python3 main.py -dataset %s -model %s --num-rounds %s --eval-every %s --clients-per-round %s --num-tips %s --sample-size %s --reference-avg-top %s --target-accuracy %s -lr %s"
-    parameters = (p['dataset'], p['model'], p['num_rounds'], p['eval_every'], p['clients_per_round'], p['num_tips'], p['sample_size'], p['reference_avg_top'], p['target_accuracy'], p['learning_rate'])
+    command = "python3 main.py -dataset %s -model %s --num-rounds %s --eval-every %s --clients-per-round %s --num-tips %s --sample-size %s --reference-avg-top %s --target-accuracy %s -lr %s --poison-type %s --poison-fraction %s --poison-from %s"
+    parameters = (p['dataset'], p['model'], p['num_rounds'], p['eval_every'], p['clients_per_round'], p['num_tips'], p['sample_size'], p['reference_avg_top'], p['target_accuracy'], p['learning_rate'], p['poison_type'], p['poison_fraction'], p['poison_from'])
     command = command % parameters
     with open(file_name, 'a+') as file:
         file.write('\n\n' + command + '\n')
